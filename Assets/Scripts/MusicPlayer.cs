13892f71b44069f212e9ae6d9d81eb65f92f5cf2
DanPaddock <danpaddk@gmail.com>
2019-12-18 17:01:13 -0500

    music working in singleton pattern

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        int num_players = FindObjectsOfType<MusicPlayer>().Length;
+        if(num_players > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
 }
